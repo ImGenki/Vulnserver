a4660f0726a0c206e79eefe8d3135dce7ef1dbc8
imgenki <thomascalabre@orange.fr>
2022-10-18 22:57:14 +0200

    test

---
+++ fuzz_vulnserver.py
@@ -0,0 +1,21 @@
+import socket
+
+buffer=["A"]
+counter=5
+while len(buffer) <= 1000:
+    buffer.append("A" * counter)
+    counter+=20
+commands=["TRUN ."]
+
+for command in commands:
+    for string in buffer:
+        print("Fuzzing" + command + str(len(string)))
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connect=s.connect(("IP adress",9999))
+        s.recv(1024)
+        s.send(bytes(command+""+string+'\r\n'))
+        s.recv(1024)
+        s.send(bytes('QUIT\r\n'))
+        s.close()
+
+
